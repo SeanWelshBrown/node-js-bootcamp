4194305b8d1a35545b67c2ebce53cda9926c8c47
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-10-19 14:32:47 -0400

    Implemented dynamic routing; read files and completed views; project complete

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,6 +3,74 @@ const http = require('http');
 const url = require('url');
 
 // ------------------------------------
+// SERVER
+
+// Data
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// Helper functions
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+
+// Server/Paths
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map(element => replaceTemplate(tempCard, element)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+
+  // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+
+  // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' })
+    res.end(data);
+
+  // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>This page cannot be found.</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log("Listening to requests on port 8000");
+});
+
+
+
+// ------------------------------------
 // FILES
 
 // Blocking, synchronous way
@@ -29,33 +97,3 @@ const url = require('url');
 // });
 // console.log("Will read file!");
 
-
-// ------------------------------------
-// SERVER
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);
-
-const server = http.createServer((req, res) => {
-  const pathName = req.url
-
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview.');
-  } else if (pathName === '/product') {
-    res.end('This is the product.')
-  } else if (pathName === '/api') {
-    res.writeHead(200, { 'Content-type': 'application/json' })
-    res.end(data);
-  } else {
-    res.writeHead(404, {
-      'Content-type': 'text/html',
-      'my-own-header': 'hello-world'
-    });
-    res.end('<h1>This page cannot be found.</h1>');
-  }
-
-});
-
-server.listen(8000, '127.0.0.1', () => {
-  console.log("Listening to requests on port 8000");
-});
