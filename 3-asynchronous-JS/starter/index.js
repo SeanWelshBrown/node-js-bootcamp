b860a0d64eded2a3c379a7492b9fe17fcd5be528
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-10-27 17:48:33 -0400

    Completed abstracting Callback Hell to Promisified functions

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,20 +1,39 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`./dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        console.log("Random dog image saved to file.");
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file 😥");
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not write the file 😥");
+      resolve("Success!");
+    });
+  });
+};
+
+readFilePro(`./dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random dog image saved to file.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 // -------------------------------------
 
